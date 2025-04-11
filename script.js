b8baceea19a9aec56bb982be1213d2fcf2cf9368
Teriyaki-Kun <michhu1212@gmail.com>
2025-04-11 19:53:40 +0700

    sad

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch("https://24252-if21-pw1-omega.vercel.app/javascript/wilayah.json")
 .then(response=> response.json())
 .then(data => {console.log(data);
 data.forEach( (item) => {
-document.getElementById("list-desa").innerHTML += `<li onclick=detail("${item.kode}")>${item.kode} ${item.nama} </li>`
+document.getElementById("list-desa").innerHTML += `<li class="list-group-item" onclick='detail("${item.kode}")'>${item.kode} ${item.nama} </li>`
 })
 })
 
@@ -19,7 +19,21 @@ fetch(`https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=${kode}`)
     data.data[0].cuaca.forEach( (item) =>{
         console.log(item);
         item.forEach((cuaca) => {
-            document.getElementById("list-cuaca").innerHTML += `<li>${cuaca.weather_desc} ${cuaca.utc_datetime}</li>`
+            document.getElementById("list-cuaca").innerHTML +=`
+            <div class="col-lg-3 p-2">
+                <div class="card">
+            <div class="card" style ="width : 18 rem;">
+                <img src="${cuaca.image}" class = "card-img-top" alt = "..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">${cuaca.weather_desc}</h5>
+                    <p class="card-text">${cuaca.utc_datetime}</p>
+                </div>
+                </div>
+                </div>
+                </div>
+                `
+                
+            
 
         })
         
